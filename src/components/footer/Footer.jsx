2acb2d4abfad2986a5e5e86ea7fd7a63b3335c5f
BKB <blainekb94@gmail.com>
2022-10-16 22:42:39 -0500

    finished footer and contact components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,23 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">Blaine Brady</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__copyright">
+        <small>
+          &copy; Blaine Brady. All rights reserved. 2022.
+        </small>
+      </div>
+    </footer>
   )
 }
 
