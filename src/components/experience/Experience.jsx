4376cfb6654ea88cc53f7399ec6fc01446e7c5e0
BKB <blainekb94@gmail.com>
2022-10-16 23:20:30 -0500

    fixed indentations and finished experience and portfolio components

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,7 +3,32 @@ import './experience.css'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h2>Skills and Knowledge</h2>
+      <div className="container experience__container">
+        <div className="frontend">
+            <h2>Frontend</h2>
+            <div className="experience__content">
+              <article className='experience__details'>
+                <h4>
+                  HTML5, CSS, JQuery, JavaScript, Bootstrap, AJAX, SASS
+                </h4>
+            </article>
+            </div>
+        </div>
+
+        <div className="backend">
+          <h2>Backend</h2>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <h4>
+                MySQL, MongoDB, Express.js, React.js, Node.js, Handlebars, Webpack
+              </h4>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 import './portfolio.css'
+import RhymeBlockIMG from '../../assets/RhymeBlock_Presentation_Preview.jpg'
+import SongSuggestIMG from '../../assets/SongSuggest_Presentation.png'
+import MusicMeetupIMG from '../../assets/MusicMeetupPic.png'
+
+const data = [
+  {
+    id: 1,
+    image: RhymeBlockIMG,
+    title: 'Rhyme Block',
+    github: "https://github.com/BlaineKB/Rhyme-Block",
+    demo: "https://blainekb.github.io/Rhyme-Block/"
+  },
+  {
+    id: 2,
+    image: SongSuggestIMG,
+    title: 'SongSuggest',
+    github: "https://github.com/BlaineKB/songsearch",
+    demo: "https://vast-stream-48863.herokuapp.com/"
+  },
+  {
+    id: 3,
+    image: MusicMeetupIMG,
+    title: 'Music Meetup',
+    github: "https://github.com/luketorv/music-meetup2",
+    demo: "https://boiling-plateau-66922.herokuapp.com/"
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      
+    <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+      {
+        data.map(({id, image, title, github, demo}) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title}/>
+              </div>
+              <h3> {title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank"> Github</a>
+                <a href={demo} className="btn btn-primary" target="_blank"> Website</a>
+              </div>
+            </article>
+        )
+      })
+     } 
+    </div>
+    </section>
   )
 }
 
